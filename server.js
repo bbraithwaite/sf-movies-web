78ffe872ec5bbf46c33d73ffb1d3665a11a0d92b
bbraithwaite <bradley.braithwaite@gmail.com>
2015-02-25 22:44:28 +0100

    intial page of bad style index.html

---
+++ server.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+
+var restify = require('restify');
+var packageConfig = require('./package');
+var port = (process.env.PORT || 4000);
+
+/**
+ * Create server object.
+ **/
+var server = restify.createServer({
+  name: packageConfig.name,
+  version: packageConfig.version
+});
+
+/**
+ * Register Middleware.
+ **/
+server.use(restify.CORS());
+
+/**
+ * Register static resources.
+ **/
+server.get(/\/*/, restify.serveStatic({
+  directory: './app/',
+  default: 'index.html'
+
+}));
+
+/**
+ * Start listening on server.
+ **/
+server.listen(port, function listen() {
+	console.log('Server running at http://127.0.0.1:' + port);	
+});
